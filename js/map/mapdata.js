095e6c62abfa3d18b4bb520ba07dbf52b4d4add4
Graf <cyrine.graf@hp.com>
2024-02-14 20:30:56 +0100

    first commit

---
+++ js/map/mapdata.js
@@ -0,0 +1,415 @@
+var simplemaps_europemap_mapdata={
+  main_settings: {
+    //General settings
+		width: "responsive", //or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    popups: "detect",
+    
+		//State defaults
+		state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "https://simplemaps.com",
+    border_size: 1.5,
+    border_color: "#ffffff",
+    all_states_inactive: "no",
+    all_states_zoomable: "no",
+    
+		//Location defaults
+		location_description: "Location description",
+    location_color: "#0096d6",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 25,
+    location_type: "marker",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "yes",
+    all_locations_hidden: "no",
+    
+		//Label defaults
+		label_color: "#ffffff",
+    label_hover_color: "#ffffff",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+   
+		//Zoom settings
+		manual_zoom: "yes",
+    back_image: "no",
+    arrow_box: "no",
+    navigation_size: "40",
+    navigation_color: "#f7f7f7",
+    navigation_border_color: "#636363",
+    initial_back: "no",
+    initial_zoom: -1,
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+		//Popup settings
+		popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+		//Advanced settings
+		div: "map",
+    auto_load: "yes",
+    rotate: "0",
+    url_new_tab: "yes",
+    images_directory: "default",
+    import_labels: "no",
+    fade_time: 0.1,
+    link_text: "View Website"
+  },
+  state_specific: {
+    AD: {
+      name: "Andorra",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    AL: {
+      name: "Albania",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    AM: {
+      name: "Armenia",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    AT: {
+      name: "Austria",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    BA: {
+      name: "Bosnia and Herzegovina",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    BE: {
+      name: "Belgium",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    BG: {
+      name: "Bulgaria",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    BY: {
+      name: "Belarus",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    CH: {
+      name: "Switzerland",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    CY: {
+      name: "Cyprus",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    CZ: {
+      name: "Czech Republic",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    DE: {
+      name: "Germany",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    DK: {
+      name: "Denmark",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    EE: {
+      name: "Estonia",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    ES: {
+      name: "Spain",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    FI: {
+      name: "Finland",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    FR: {
+      name: "France",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "#fr"
+    },
+    GB: {
+      name: "United Kingdom",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    GE: {
+      name: "Georgia",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    GR: {
+      name: "Greece",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    HR: {
+      name: "Croatia",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    HU: {
+      name: "Hungary",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    IE: {
+      name: "Ireland",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    IS: {
+      name: "Iceland",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    IT: {
+      name: "Italy",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    LI: {
+      name: "Liechtenstein",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    LT: {
+      name: "Lithuania",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    LU: {
+      name: "Luxembourg",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    LV: {
+      name: "Latvia",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    MD: {
+      name: "Moldova",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    ME: {
+      name: "Montenegro",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    MK: {
+      name: "Macedonia",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    NL: {
+      name: "Netherlands",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    NO: {
+      name: "Norway",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    PL: {
+      name: "Poland",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    PT: {
+      name: "Portugal",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    RO: {
+      name: "Romania",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    RS: {
+      name: "Serbia",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    SE: {
+      name: "Sweden",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    SI: {
+      name: "Slovenia",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    SK: {
+      name: "Slovakia",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    TR: {
+      name: "Turkey",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    UA: {
+      name: "Ukraine",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    XK: {
+      name: "Kosovo",
+      description: "",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    }
+  },
+  locations: {
+    "0": {
+      lat: 48.857,
+      lng: 2.341,
+    },
+    "1": {
+      lat: 46.993044,
+      lng:  7.444764,
+    },
+    "2": {
+      lat: 50.841139,
+      lng:  4.368662,
+    },
+    "3": {
+      lat: 52.504261, 
+      lng:  -1.888577,
+    },
+    "4": {
+      lat: 43.141577,  
+      lng:  12.355041, 
+    },
+    "5": {
+      lat: 51.536443, 
+      lng:  9.923979,  
+    },
+
+ 
+  },
+  labels: {}
+};
+
+
